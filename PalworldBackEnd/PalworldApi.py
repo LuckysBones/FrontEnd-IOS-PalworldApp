9b8537caae5225df7b64a06c9fda4581b9aacfa5
LuckysBones <77938956+LuckysBones@users.noreply.github.com>
2024-05-17 23:54:34 -0600

    added restapit fuctions

--- PalworldBackEnd/PalworldApi.py
+++ PalworldBackEnd/PalworldApi.py
@@ -9,11 +9,46 @@ class PalworldRestApi:
     def findPlayers():
         url = "http://admin:AlecSucks@localhost:8212/v1/api/players"
         payload={}
-        headers = {
-                'Accept': 'application/json'
-                }
+        headers = {'Accept': 'application/json'}
         response = requests.request("GET", url, headers=headers, data=payload)
         return response.text
 
     def findMetrics():
-        print("findMetrics")
+        url = "http://admin:AlecSucks@localhost:8212/v1/api/metrics"
+        payload={}
+        headers = {'Accept': 'application/json'}
+        response = requests.request("GET", url, headers=headers, data=payload)
+        return response.text
+    
+    def sendMessage(message):
+        url = "http://admin:AlecSucks@localhost:8212/v1/api/announce"
+        payload = json.dumps({"message": message})
+        headers = {'Content-Type': 'application/json'}
+        response = requests.request("POST", url, headers=headers, data=payload)
+        return response.text
+
+    def serverInfo():
+        url = "http://admin:AlecSucks@localhost:8212/v1/api/info"
+        payload={}
+        headers = {'Accept': 'application/json'}
+        response = requests.request("GET", url, headers=headers, data=payload)
+        return response.text
+    
+    def saveWorld():
+        url = "http://localhost:8212/v1/api/save"
+        payload={}
+        headers = {}
+        response = requests.request("POST", url, headers=headers, data=payload)
+        return response.text
+    
+    def shutDown(shutdownMessage,shutdownTime):
+        url = "http://localhost:8212/v1/api/shutdown"
+        payload = json.dumps({
+            "waittime": shutdownTime,
+            "message": shutdownMessage
+            })
+        headers = {
+            'Content-Type': 'application/json'
+            }
+        response = requests.request("POST", url, headers=headers, data=payload)
+        return response.text
