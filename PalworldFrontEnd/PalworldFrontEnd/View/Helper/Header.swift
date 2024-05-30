d649ef885094e07de2523a9e4d26f666cf5af677
Luc Gremillion <LuckysBones@gmail.com>
2024-05-29 23:33:10 -0600

    Rough First Edit of Front End

---
+++ PalworldFrontEnd/PalworldFrontEnd/View/Helper/Header.swift
@@ -0,0 +1,18 @@
+//
+//  Header.swift
+//  PalworldFrontEnd
+//
+//  Created by Luc Gremillion on 5/29/24.
+//
+
+import SwiftUI
+
+struct Header: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Header()
+}
