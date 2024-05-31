9ba1b1839c1888c602ac57013c97bcd6a04b0117
Luc Gremillion <LuckysBones@gmail.com>
2024-05-30 22:58:10 -0600

    Additional updates to front end and navigation start

---
+++ PalworldFrontEnd/PalworldFrontEnd/View/Helper/Controls.swift
@@ -0,0 +1,18 @@
+//
+//  Controls.swift
+//  PalworldFrontEnd
+//
+//  Created by Luc Gremillion on 5/30/24.
+//
+
+import SwiftUI
+
+struct Controls: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Controls()
+}
