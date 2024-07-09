844378a29135a51a7a0f379bab522809850e13db
Luc Gremillion <LuckysBones@gmail.com>
2024-07-08 23:07:55 -0600

    SSL Update

--- PalLensBackEnd/PalLensBackEnd/settings.py
+++ PalLensBackEnd/PalLensBackEnd/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = ['localhost', '127.0.0.1','[::1]']
 # Application definition
 
 INSTALLED_APPS = [
+    'django_extensions',
+    'sslserver',
     'daphne',
     'channels',
     'PalSockets',
@@ -136,3 +138,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+SECURE_SSL_REDIRECT = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
--- PalLensBackEnd/Tests/full-call-test.py
+++ PalLensBackEnd/Tests/full-call-test.py
@@ -2,7 +2,7 @@ import asyncio
 import websockets
 
 async def players():
-    uri = "ws://localhost:8000/ws/players/"  # Replace with your WebSocket server URI
+    uri = "ws://localhost:8001/ws/players/"  # Replace with your WebSocket server URI
     async with websockets.connect(uri) as websocket:
             await websocket.send('')
             message = await websocket.recv()
@@ -11,7 +11,7 @@ async def players():
 
 
 async def info():
-    uri = "ws://localhost:8000/ws/info/"  # Replace with your WebSocket server URI
+    uri = "ws://localhost:8001/ws/info/"  # Replace with your WebSocket server URI
     async with websockets.connect(uri) as websocket:
             await websocket.send('')
             message = await websocket.recv()
@@ -20,7 +20,7 @@ async def info():
 
 
 async def metric():
-    uri = "ws://localhost:8000/ws/metric/"  # Replace with your WebSocket server URI
+    uri = "ws://localhost:8001/ws/metric/"  # Replace with your WebSocket server URI
     async with websockets.connect(uri) as websocket:
             await websocket.send('')
             message = await websocket.recv()
@@ -29,21 +29,21 @@ async def metric():
 
 
 async def message():
-    uri = "ws://localhost:8000/ws/message/"  # Replace with your WebSocket server URI
+    uri = "ws://localhost:8001/ws/message/"  # Replace with your WebSocket server URI
     async with websockets.connect(uri) as websocket:
             await websocket.send("message")
             await websocket.close()
 
 
 async def save():
-    uri = "ws://localhost:8000/ws/save/"  # Replace with your WebSocket server URI
+    uri = "ws://localhost:8001/ws/save/"  # Replace with your WebSocket server URI
     async with websockets.connect(uri) as websocket:
             await websocket.send("")
             await websocket.close()
 
 
 async def shutdown():
-    uri = "ws://localhost:8000/ws/shutdown/"  # Replace with your WebSocket server URI
+    uri = "ws://localhost:8001/ws/shutdown/"  # Replace with your WebSocket server URI
     async with websockets.connect(uri) as websocket:
             await websocket.send("message")
             await websocket.close()
